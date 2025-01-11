058ea9c7ed5aba1610f84a6ff5e2742776a6554c
Ben-abdurrehman <abdurrehman12k6@gmail.com>
2025-01-11 23:11:54 +0500

    Qr-code Generating functionality added, it's my forth commit!

--- Qr-code-generator/src/App.jsx
+++ Qr-code-generator/src/App.jsx
@@ -1,39 +1,88 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { Input } from '@headlessui/react'
-import validator from 'validator';
-import 'react-toastify/dist/ReactToastify.css';
-import { toast, ToastContainer } from 'react-toastify';
+import React, { useState, useRef, useEffect } from "react";
+import { Input } from "@headlessui/react";
+import validator from "validator";
+import { toast, ToastContainer } from "react-toastify";
+import QRCode from "react-qr-code";
 
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const [url, setUrl] = useState("ben")
-  
+  const [url, setUrl] = useState("");
+  // const [check, setCheck] = useState(false)
+
   const handleSubmit = () => {
-    if(validator.isURL(url))
-    {
-      toast.success('Valid URL! Your QR-Code will generate shortly', {
-        position: 'top-center',
+    if (validator.isURL(url)) {
+      toast.success("Valid URL! Your QR-Code will generate shortly", {
+        position: "top-center",
         autoClose: 3000,
-        theme: 'dark'
+        theme: "dark",
       });
     } else {
-      toast.error('Invalid URL! Please enter a correct one to generate its QR-Code', {
-        position: 'top-center',
-        autoClose: 3000,
-        theme: 'dark'
-      });
-  }
-}
+      toast.error(
+        "Invalid URL! Please enter a correct one to generate its QR-Code",
+        {
+          position: "top-center",
+          autoClose: 3000,
+          theme: "dark",
+        }
+      );
+    }
+  };
   return (
-    <div style={{width: "90vw", height: "90vh", paddingBlock: "30px", paddingInline: "50px", backgroundColor: "#3b3b3b", borderRadius: "10px"}}>
-      <h1 style={{ color: "white", fontSize: "4rem", fontWeight: "800"}}>
-        Please enter the URL below!
-      </h1>
-      <Input style={{width: "45%", borderRadius: "10px", paddingBlock: "20px", paddingInline: "20px", backgroundColor: "#1c1c1c", border: "none", fontSize: "1.5rem"}} type='text' onChange={e => setUrl(e.target.value)}></Input>
-      <button onClick={handleSubmit}>Submit</button>
-      <ToastContainer/>
+    <div
+      style={{
+        width: "90vw",
+        height: "90vh",
+        paddingBlock: "30px",
+        paddingInline: "50px",
+        backgroundColor: "#3b3b3b",
+        borderRadius: "10px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "50px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={{ color: "white", fontSize: "4rem", fontWeight: "800", textAlign: "center" }}>
+          Please enter the URL below!
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Input
+            style={{
+              width: "45%",
+              borderRadius: "10px",
+              paddingBlock: "20px",
+              paddingInline: "20px",
+              backgroundColor: "#1c1c1c",
+              border: "none",
+              fontSize: "1.5rem",
+            }}
+            type="text"
+            onChange={(e) => setUrl(e.target.value)}
+          ></Input>
+          <button style={{ padding: "20px" }} onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+        <ToastContainer />
+      </div>
+      <div>{validator.isURL(url) && <QRCode value={url} size={256} />}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
